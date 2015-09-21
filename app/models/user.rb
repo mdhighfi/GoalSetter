b8b5d9243d50d99af94b47f1e8ab110cd0796279
Matt Highfield <matthew.highfield@gmail.com>
2015-09-21 14:16:40 -0700

    Create user model

---
+++ app/models/user.rb
@@ -0,0 +1,39 @@
+class User < ActiveRecord::Base
+  validates :username, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  attr_reader :password
+
+  after_initialize :ensure_session_token
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password).to_s
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by_username(username)
+
+    return nil if user.nil? || !user.is_password?(password)
+    user
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def self.generate_session_token
+    SecureRandom.base64
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    save!
+    session_token
+  end
+end
--- spec/features/auth_spec.rb
+++ spec/features/auth_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'rails_helper'
 
 feature "the signup process" do
-  given(:user) { build(:user) }
+  given(:user1) { build(:user) }
 
   scenario "has a new user page" do
     visit new_user_url
@@ -13,47 +13,47 @@ feature "the signup process" do
   feature "signing up a user" do
     scenario "shows username on the homepage after signup" do
       visit new_user_url
-      fill_in "Username", with: user.username
-      fill_in "Password", with: user.password
+      fill_in "Username", with: user1.username
+      fill_in "Password", with: user1.password
       click_button "Sign Up"
 
-      expect(page).to have_content(user.username)
+      expect(page).to have_content(user1.username)
     end
 
   end
 end
 
 feature "logging in" do
-  given(:user) { create(:user) }
+  given(:user2) { create(:user) }
 
   scenario "shows username on the homepage after login" do
     visit new_session_url
-    fill_in "Username", with: user.username
-    fill_in "Password", with: user.password
+    fill_in "Username", with: user2.username
+    fill_in "Password", with: user2.password
     click_button "Log In"
 
-    expect(page).to have_content(user.username)
+    expect(page).to have_content(user2.username)
   end
 
 end
 
 feature "logging out" do
-  given(:user) { create(:user) }
+  given(:user3) { create(:user) }
 
   scenario "begins with logged out state" do
-    visit user_url(user)
+    visit user_url(user3)
 
     expect(page).to_not have_content("Log Out")
   end
 
   scenario "doesn't show username on the homepage after logout" do
     visit new_session_url
-    fill_in "Username", with: user.username
-    fill_in "Password", with: user.password
+    fill_in "Username", with: user3.username
+    fill_in "Password", with: user3.password
     click_button "Log In"
     click_button("Log Out")
 
-    expect(page).to_not have_content(user.username)
+    expect(page).to_not have_content(user3.username)
   end
 
 end
