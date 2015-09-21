75123fd174c4b4cb3821195a1afdabd21bdf961b
Matt Highfield <matthew.highfield@gmail.com>
2015-09-21 14:35:53 -0700

    Create sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,26 @@
 class SessionsController < ApplicationController
+  def new
+    render :new
+  end
+
+  def create
+    user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if user.nil?
+      flash.now[:errors] = ["Bad username / password combo"]
+      render :new
+    else
+      log_in!(user)
+      redirect_to user_url(user)
+    end
+  end
+
+  def destroy
+    current_user.reset_session_token!
+    session[:session_token] = nil
+    redirect_to new_session_url
+  end
 end
